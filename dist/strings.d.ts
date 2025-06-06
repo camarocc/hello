f6698acc4debb93a69c0f4cb5fbcb39efba25903
luochao <luochao@meferi.cn>
2025-06-06 15:11:58 +0800

    11

---
+++ dist/strings.d.ts
@@ -0,0 +1,4 @@
+import type { KeyValueItem } from './types.js';
+export declare const LOCALES_TRANSLATIONS: KeyValueItem;
+export declare const LOCAL_SEARCH_TRANSLATIONS: KeyValueItem;
+export declare const ALGOLIA_SEARCH_TRANSLATIONS: KeyValueItem;
